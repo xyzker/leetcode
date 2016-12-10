a035a2ba8ad1ddc5d114e700c74aed826be16252
xyzker <410317669@qq.com>
2016-12-10 15:44:29 +0800

    add new solutions

--- src/answer/Solution.java
+++ src/answer/Solution.java
@@ -2602,10 +2602,23 @@ public class Solution {
 		}
 		return true;
 	}
+
+	// you need treat n as an unsigned value
+	public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n >>>= 1;   // CATCH: must do unsigned shift
+            if (i < 31) // CATCH: for last digit, don't shift!
+                result <<= 1;
+        }
+        return result;
+	}
   
 	public static void main(String[] args){
 		Solution s = new Solution();
-		boolean res = s.isAnagram("anagram","nagaram");
-		System.out.println(res);
+		System.out.println(s.reverseBits(43261596));
+        System.out.println(Integer.reverse(43261596));
+
 	}
 }
