17bfbdeef55c5a72f20165cf79348ed615313d02
柯希 <kexi@qiyi.com>
2017-01-23 18:13:59 +0800

    new Solutions

--- src/answer/Solution.java
+++ src/answer/Solution.java
@@ -2614,11 +2614,41 @@ public class Solution {
         }
         return result;
 	}
+
+	public List<String> wordBreak(String s, List<String> wordDict) {
+		return DFS(s, wordDict, new HashMap<>());
+	}
+
+	// DFS function returns an array including all substrings derived from s.
+	List<String> DFS(String s, List<String> wordDict, HashMap<String, LinkedList<String>>map) {
+		if (map.containsKey(s))
+			return map.get(s);
+
+		LinkedList<String> res = new LinkedList<>();
+		if (s.length() == 0) {
+			res.add("");
+			return res;
+		}
+		for (String word : wordDict) {
+			if (s.startsWith(word)) {
+				List<String> sublist = DFS(s.substring(word.length()), wordDict, map);
+				for (String sub : sublist)
+					res.add(word + (sub.isEmpty() ? "" : " ") + sub);
+			}
+		}
+		map.put(s, res);
+		return res;
+	}
+
+	public boolean isUgly(int num) {
+		for (int i=2; i<6 && num>0; i++)
+			while (num % i == 0)
+				num /= i;
+		return num == 1;
+	}
   
 	public static void main(String[] args){
 		Solution s = new Solution();
-		System.out.println(s.reverseBits(43261596));
-        System.out.println(Integer.reverse(43261596));
-
+		System.out.println(s.isUgly(14));
 	}
 }
