ce9a47112d29901d6716e691466098d7fe86498b
xyzker <410317669@qq.com>
2017-01-27 22:06:14 +0800

    add new solutions

--- src/answer/Solution.java
+++ src/answer/Solution.java
@@ -2692,9 +2692,30 @@ public class Solution {
 		}
 		return count == numCourses;
 	}
+
+	public int[] intersection(int[] nums1, int[] nums2) {
+		Set<Integer> set = new HashSet<>();
+		Set<Integer> intersect = new HashSet<>();
+		for (int i = 0; i < nums1.length; i++) {
+			set.add(nums1[i]);
+		}
+		for (int i = 0; i < nums2.length; i++) {
+			if (set.contains(nums2[i])) {
+				intersect.add(nums2[i]);
+			}
+		}
+		int[] result = new int[intersect.size()];
+		int i = 0;
+		for (Integer num : intersect) {
+			result[i++] = num;
+		}
+		return result;
+	}
+
+
   
 	public static void main(String[] args){
 		Solution s = new Solution();
-		System.out.println(s.isPerfectSquare(2147483647));
+		System.out.println(s.intersection(new int[]{1, 2}, new int[]{1,1}));
 	}
 }
