4a592876229f37ce37953dbbe2f87b56dec23260
xyzker <410317669@qq.com>
2017-01-27 22:25:36 +0800

    add new solutions

--- src/answer/Solution.java
+++ src/answer/Solution.java
@@ -2712,6 +2712,20 @@ public class Solution {
 		return result;
 	}
 
+	public int integerReplacement(int n) {
+		int c = 0;
+		while (n != 1) {
+			if ((n & 1) == 0) {
+				n >>>= 1;
+			} else if (n == 3 || ((n >>> 1) & 1) == 0) {
+				--n;
+			} else {
+				++n;
+			}
+			++c;
+		}
+		return c;
+	}
 
   
 	public static void main(String[] args){
