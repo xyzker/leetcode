b9e9ead903d6c65778f8fa0523a6129cdfe3a521
xyzker <410317669@qq.com>
2017-04-01 16:36:42 +0800

    new Solutions

--- src/answer/Solution.java
+++ src/answer/Solution.java
@@ -2800,6 +2800,31 @@ public class Solution {
         return head;
     }
 
+	/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+	public int guessNumber(int n) {
+		int lo = 1, hi = n, mid;
+		while(lo < hi){
+			mid = lo + (hi-lo)/2;
+			int res = guess(mid);
+			if(res == 0)
+				return mid;
+			else if(res == -1){
+				hi = mid - 1;
+			}else{
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private int guess(int num){
+		//TODO
+		return 0;
+	}
+
 	public static void main(String[] args){
 		Solution s = new Solution();
 		System.out.println(s.lastRemaining(9));
