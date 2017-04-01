1e87798e42c41b368eb07910e083dab08f49f9b9
xyzker <410317669@qq.com>
2017-04-01 18:07:18 +0800

    new Solutions

--- src/answer/Solution.java
+++ src/answer/Solution.java
@@ -2825,6 +2825,14 @@ public class Solution {
 		return 0;
 	}
 
+    public boolean hasPathSum(TreeNode root, int sum) {
+	    if(root == null) return false;
+        if(root.left == null && root.right == null)
+            return root.val == sum;
+        return hasPathSum (root.left, sum - root.val)
+               || hasPathSum(root.right, sum - root.val);
+    }
+
 	public static void main(String[] args){
 		Solution s = new Solution();
 		System.out.println(s.lastRemaining(9));
