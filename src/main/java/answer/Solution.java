2193401b008aef24b6f207259f2b4dd23f270d52
xyzker <410317669@qq.com>
2017-08-13 21:58:39 +0800

    add new solutions

--- src/main/java/answer/Solution.java
+++ src/main/java/answer/Solution.java
@@ -3170,6 +3170,30 @@ public class Solution {
 		return lives;
 	}
 
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+
+		final Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+
+		while(!queue.isEmpty()) {
+			final TreeNode node = queue.poll();
+			final TreeNode left = node.left;
+			node.left = node.right;
+			node.right = left;
+
+			if(node.left != null) {
+				queue.offer(node.left);
+			}
+			if(node.right != null) {
+				queue.offer(node.right);
+			}
+		}
+		return root;
+	}
+
 	public static void main(String[] args){
 		Solution s = new Solution();
 		int[][] board = new int[][]{{0,0,0,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,0,0,0}};
