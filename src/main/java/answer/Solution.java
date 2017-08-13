bf938de5cddd991b957467ca2be59eece2df6740
xyzker <410317669@qq.com>
2017-08-13 22:29:22 +0800

    add new solutions

--- src/main/java/answer/Solution.java
+++ src/main/java/answer/Solution.java
@@ -3194,11 +3194,22 @@ public class Solution {
 		return root;
 	}
 
+	public List<String> fizzBuzz(int n) {
+		List<String> res = new ArrayList<>(n);
+		for(int i=1; i<=n; i++){
+			if(i % 3 == 0 && i % 5 == 0) res.add("FizzBuzz");
+			else if(i % 3 == 0) res.add("Fizz");
+			else if(i % 5 == 0) res.add("Buzz");
+			else res.add(String.valueOf(i));
+		}
+		return res;
+	}
+
 	public static void main(String[] args){
 		Solution s = new Solution();
-		int[][] board = new int[][]{{0,0,0,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,0,0,0}};
-		s.gameOfLife(board);
-		System.out.println(board);
+		/*int[][] board = {{0,0,0,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,0,0,0}};
+		s.gameOfLife(board);*/
+		System.out.println(s.fizzBuzz(15));
 
 	}
 }
