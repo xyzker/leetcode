c5e7f6d61c77a6a2dae010f60caf908493c77e26
xyzker <410317669@qq.com>
2017-09-03 21:53:29 +0800

    add new solutions

--- src/main/java/answer/Solution.java
+++ src/main/java/answer/Solution.java
@@ -3205,11 +3205,36 @@ public class Solution {
 		return res;
 	}
 
+	public int hIndex(int[] citations) {
+		int length = citations.length;
+		if (length == 0) {
+			return 0;
+		}
+
+		int[] array2 = new int[length + 1];
+		for (int i = 0; i < length; i++) {
+			if (citations[i] > length) {
+				array2[length] += 1;
+			} else {
+				array2[citations[i]] += 1;
+			}
+		}
+		int t = 0;
+
+		for (int i = length; i >= 0; i--) {
+			t = t + array2[i];
+			if (t >= i) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
 	public static void main(String[] args){
 		Solution s = new Solution();
 		/*int[][] board = {{0,0,0,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,0,0,0}};
 		s.gameOfLife(board);*/
-		System.out.println(s.fizzBuzz(15));
+		System.out.println(s.hIndex(new int[]{1,2,2}));
 
 	}
 }
