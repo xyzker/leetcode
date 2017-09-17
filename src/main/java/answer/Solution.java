a5e9c8e1c94eb8c9cee72b0c38ef4d850679b238
xyzker <410317669@qq.com>
2017-09-17 17:40:06 +0800

    add new solutions

--- src/main/java/answer/Solution.java
+++ src/main/java/answer/Solution.java
@@ -3273,6 +3273,27 @@ public class Solution {
 		return list.val == 0 ? list.next : list;
 	}
 
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<>();
+		List<List<Integer>> wrapList = new LinkedList<>();
+
+		if(root == null) return wrapList;
+
+		queue.offer(root);
+		while(!queue.isEmpty()){
+			int levelNum = queue.size();
+			List<Integer> subList = new LinkedList<>();
+			for(int i=0; i<levelNum; i++) {
+				if(queue.peek().left != null) queue.offer(queue.peek().left);
+				if(queue.peek().right != null) queue.offer(queue.peek().right);
+				subList.add(queue.poll().val);
+			}
+			wrapList.add(subList);
+		}
+		return wrapList;
+
+	}
+
 	public static void main(String[] args){
 		Solution s = new Solution();
 		/*int[][] board = {{0,0,0,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,0,0,0}};
