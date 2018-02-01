c3d2c884eccd6f630c5ca790318a74b52e531831
xyzker <410317669@qq.com>
2018-02-01 18:21:02 +0800

    add new solutions

--- src/main/java/answer/Solution.java
+++ src/main/java/answer/Solution.java
@@ -3527,6 +3527,34 @@ public class Solution {
 		return end-start+1;
 	}
 
+	public List<String> topKFrequent(String[] words, int k) {
+		List<String> result = new LinkedList<>();
+		Map<String, Integer> map = new HashMap<>();
+		for(int i=0; i<words.length; i++)
+		{
+			if(map.containsKey(words[i]))
+				map.put(words[i], map.get(words[i])+1);
+			else
+				map.put(words[i], 1);
+		}
+
+		PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+				(a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+		);
+
+		for(Map.Entry<String, Integer> entry: map.entrySet())
+		{
+			pq.offer(entry);
+			if(pq.size()>k)
+				pq.poll();
+		}
+
+		while(!pq.isEmpty())
+			result.add(0, pq.poll().getKey());
+
+		return result;
+	}
+
 	public static void main(String[] args){
 		Solution s = new Solution();
 		//int[][] nums = {{0,1,0,0}, {1,1,1,0}, {0,1,0,0}, {1,1,0,0}};
